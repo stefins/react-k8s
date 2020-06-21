5f6721ba3f2b7f0eba40a619797eafb5d08cdda0
Stef <sstefin@bk.ru>
2020-06-21 10:11:36 -0500

    :tada: Initial commit

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+docker build -t iamstefin/muti-client:latest -t iamstefin/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t iamstefin/multi-server:latest -t iamstefin/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t iamstefin/multi-worker:latest -t iamstefin/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push "iamstefin/multi-client:latest"
+docker push "iamstefin/multi-server:latest"
+docker push "iamstefin/multi-worker:latest"
+
+export KUBECONFIG=$(pwd)/simplek8s-kubeconfig.yaml
+
+docker push iamstefin/multi-client:$SHA
+docker push iamstefin/multi-server:$SHA
+docker push iamstefin/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=iamstefin/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=iamstefin/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=iamstefin/multi-client:$SHA
